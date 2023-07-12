3d713d4b2dc878bb73d656d6e9d58c80fbd1aa30
Jens De Dycker <jensdedycker@hotmail.com>
2023-07-12 09:13:19 +0200

    End of section 2

---
+++ entities/AppUser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.entities
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string? UserName { get; set; }
+
+    }
+}
