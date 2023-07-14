b8b496d13ff7d1a98cb86725439a44e82a091ed4
Jens De Dycker <jensdedycker@hotmail.com>
2023-07-14 12:30:30 +0200

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  baseUrl = 'http://localhost:5000/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'accounts/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'accounts/register', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    );
+  }
+}
